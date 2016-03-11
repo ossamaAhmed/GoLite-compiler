71e0e867cfa9ab9dab55290e9f83b0c7ce7b9374
shabbir-hussain <mohd.husn001@gmail.com>
2016-03-11 15:34:32 -0500

    added weeder

--- src/goLite.ml
+++ src/goLite.ml
@@ -1,6 +1,7 @@
 (* main *)
 
 open Lexer
+open Weeder
 
 let _ =
     if Array.length Sys.argv > 1
@@ -11,7 +12,8 @@ let _ =
             let out = (Filename.chop_extension filename) in
             let lexbuf= Lexing.from_channel (open_in file) in
              (*Lexer.print_tokens lexbuf;*)
-             let myprog = Parser.parse Lexer.golite lexbuf in
+            let myprog = Parser.parse Lexer.golite lexbuf in
+            let weededProg = Weeder.weed myprog 0 in 
             let _ = PrettyPrinter.pretty_print myprog out in
             print_string ("Valid\n"); 
         with 
