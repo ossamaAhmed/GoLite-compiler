835e53bc35c0d72574f754a3382c2f9670355b89
shabbir-hussain <mohd.husn001@gmail.com>
2016-03-22 11:26:53 -0400

    added test cases for binary ops

---
+++ TEST_PROGRAMS/INVALID_TYPECHECK/add_stringsandints.go
@@ -0,0 +1,9 @@
+package main;
+
+func main() {
+    
+    //this operation is not allowed
+    var c int = 1;
+    var x int = c+"hello";
+    print(x);
+}
---
+++ TEST_PROGRAMS/VALID_TYPECHECK/add_strings.go
@@ -0,0 +1,9 @@
+package main;
+
+func main() {
+    
+    //this assignment is allowed
+    var c string = "1234hello";
+    var x int = c+c;
+    print(x);
+}
