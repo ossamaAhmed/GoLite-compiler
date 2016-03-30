acf392818464218b30124c0fcfb04821d70e5d18
shabbir-hussain <mohd.husn001@gmail.com>
2016-03-30 17:05:04 -0400

    added primes benchmark

---
+++ programs/benchmark/printPrimes.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//this code was benchmarked at 44.5s on a
+//Linux coreos-3 4.2.2-coreos-r2 #2 SMP Thu Feb 18 16:24:24 UTC 2016 x86_64 Intel(R) Xeon(R) CPU E5-2660 0 @ 2.20GHz GenuineIntel GNU/Linux
+func main() {
+            var primes [] int;
+            primes = append(primes,2)
+
+            for i:=2;i<1000000;i++ {
+                if(isPrime(primes,i)){
+                    primes=append(primes,i)
+                }
+            }
+            println(primes)
+}
+
+func isPrime(primes []int,test int) bool{
+
+    for _,v := range primes {
+        if test%v == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
